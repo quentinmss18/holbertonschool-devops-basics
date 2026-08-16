9fe4a76fb9ef8cc03af9727eda332303f437250d
quentinmss <quentinmoussous@gmail.com>
2026-08-16 16:20:02 +0200

    Delete holbertonschool-devops-basics-main/exploring_your_first_network directory

--- holbertonschool-devops-basics-main/exploring_your_first_network/resolve_hostname.sh
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/env bash
-getent hosts "$1"
--- holbertonschool-devops-basics-main/exploring_your_first_network/test_loopback.sh
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/env bash
-ping -c 4 $(ip -4 -brief address show scope host | tr -s ' ' | cut -d' ' -f3 | head -n 1 | cut -d'/' -f1)
